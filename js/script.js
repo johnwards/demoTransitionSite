322b7264027a38c31e77cd90e281cba962e65f7f
John Wards <johnwards@gmail.com>
2011-09-27 12:46:42 +0100

    Refactored for left and right scrolling

--- js/script.js
+++ js/script.js
@@ -5,14 +5,22 @@
 
 function resizePages() {
 	var h = $(window).height();
-	var height  =  h < 640 ? 640 : h;
+    var w = $(window).width();
+	var height  =  h < 480 ? 480 : h;
+    var width  =  w;
+    $('#container').css('width',width).css('height',height);
+    var pageContainerWidth = 0;
 	$('.page').each(function(){
         var $this = $(this);
         var currentHeight = $this.css('height').replace(/[^0-9]/gi, '');
         if(currentHeight < height) {
             $this.css('height',height);
         }
+
+        $this.css('width',width);
+        pageContainerWidth += width;
     });
+    $('#page-container').css('width',pageContainerWidth).css('height',height);
 }
 
 function pageTurn(direction) {
@@ -22,13 +30,22 @@ function pageTurn(direction) {
 	if (newPage) {
 		$('.page').removeClass('selected');
 		newPage.addClass('selected');
-		jQuery.scrollTo.window().queue([]).stop();
-		$(window).scrollTo(newPage,1200,{easing:'easeOutQuint'});
 
+        var pageNumber = newPage.attr("id").replace(/[^0-9]/gi, '');
+        var width = newPage.css('width').replace(/[^0-9]/gi, '');
+        var left = (pageNumber-1) * width;
+
+        newPage.parent().animate({'left': "-"+left+"px"}, 1200, "easeOutBack");
 	}
 }
 
 $(document).ready(function() {
+
+    $("#container").swipe( {threshold:100, swipeLeft: function(event) {
+        pageTurn("next");
+    }, swipeRight: function(event) {
+        pageTurn("prev");
+    }} );
 
     $('.page .next').click(function(e) {
 		pageTurn('next');
